256491ca414ae10769b64fc27c1fa520bf83d29b
nikita kulmodiya <kulmodiyaneek@gmail.com>
2022-11-12 14:17:49 +0530

    display show result and small fetch refector

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,15 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
   const [input, setInput] = useState('');
-
+  const [results, setResults] = useState(null);
   const onSearch = () => {
     // https://api.tvmaze.com/search/shows?q=mens
-    fetch('https://api.tvmaze.com/search/shows?q=mens')
-      .then(r => r.json())
-      .then(result => {
-        console.log(result);
-      });
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+    });
   };
   const onInputChange = ev => {
     setInput(ev.target.value);
@@ -21,6 +20,22 @@ const Home = () => {
       onSearch();
     }
   };
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No Result</div>;
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => {
+            return <div key={item.show.id}>{item.show.name}</div>;
+          })}
+        </div>
+      );
+    }
+    return null;
+  };
+
   return (
     <MainPageLayout>
       <input
@@ -32,6 +47,7 @@ const Home = () => {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
